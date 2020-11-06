71a08db5017ba76b9f4b9ff000d501e8f84ee241
elis-ctrl <elis_emb@hotmail.com>
2020-11-06 16:04:01 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,64 @@
+const cipher = {
+
+  encode: function(key, msg) {
+    let newStrCrip = ""
+
+    if ( key == null || msg == null || key == 0 || msg == 0) {
+      throw new TypeError("Deu bug"); 
+  } else {
+    for(let contador = 0; contador < msg.length; contador++) {
+
+      let posicaoDaLetraNoAlfabeto = msg.charCodeAt(contador) - 65;
+      let newcharC = ((posicaoDaLetraNoAlfabeto + key) % 26) + 65;
+      let newString = String.fromCharCode(newcharC)
+
+      newStrCrip += newString;
+    }
+    return newStrCrip
+  }
+},
+
+  decode: function(keyd, msgd) {
+    let newStrDcrip = ""
+
+    if ( keyd == null || msgd == null || keyd == 0 || msgd == 0) {
+      throw new TypeError("Deu bug"); 
+  } else {
+    for(let c = 0; c < msgd.length; c++) {
+
+      let posicaoDaLetraNoAlfabetochar = msgd.charCodeAt(c) - 90;
+      let newcharDC = ((posicaoDaLetraNoAlfabetochar - keyd) % 26) + 90;
+      let newStringD = String.fromCharCode(newcharDC)
+
+      newStrDcrip += newStringD;
+    } 
+    return newStrDcrip
+  }
+}
+};
+
+  export default cipher;
+
+
+
+
+
+
+
+// dados passados no video
+// (codigodaletra + desloc) % tamanhodoalfabeto
+// codigodaletraasc - codigodaprimeiraletra + deslocamento) % tamanho do alfabeto ) + codigodaprimeiraletra
+// strig.FromcharCodeAt()
+// console.log(cipher);
+
+// fu calculate() {
+//   var input = document.getElementById("input").value.toUpperCase();
+//   var result = "";
+
+//   for(var i = 0; i<input.length; i++){ //Passa por cada caracter do input
+
+//       var posicaoDaLetraNoAlfabeto = input.charCodeAt(i)-64; //Identifica qual letra é do alfabeto
+//       var letraComDeslocamento = (posicaoDaLetraNoAlfabeto + key) % 26; //Faz o deslocamento de César e mantém dentro do alfabeto (26 letras)
+//       letraComDeslocamento = letraComDeslocamento == 0 ? 26 : letraComDeslocamento; //MOD retornar 0 caso o resultado seja 26, tem que tratar isso
+//       result += values[letraComDeslocamento-1]; //Faz -1 porque a letra 1 (A) está no índice 0 do teu array.
+//   }
---
+++ index.js
@@ -0,0 +1,39 @@
+import cipher from './cipher.js';
+
+const bc = document.getElementById("bc");
+bc.addEventListener("click", cifrarMensagem);
+
+function cifrarMensagem(event) {
+    event.preventDefault();
+    let msg = document.getElementById("msgcrip").value.toUpperCase();
+    let key = document.getElementById("key").value;
+    const stringCifrada = cipher.encode(Number(key), msg);
+    const exibeStringCifrada = document.getElementById("newStrCrip");
+    exibeStringCifrada.innerHTML = stringCifrada
+}
+
+const bd = document.getElementById("bd");
+bd.addEventListener("click", decifrarMensagem); 
+
+function decifrarMensagem(event){
+    event.preventDefault();
+    let msgd = document.getElementById("msgdcrip").value.toUpperCase();
+    let keyd = document.getElementById("keyd").value;
+    const stringDecifrada = cipher.decode(Number(keyd), msgd);
+    const exibeStringDecifrada = document.getElementById("newStrDcrip");
+    exibeStringDecifrada.innerHTML = stringDecifrada
+}
+
+
+    // function Decode(){
+    //      console.log("chamou o botão encrip")
+    // }
+
+    // function Encode(){
+    //     console.log("chamou o botão decrip")
+
+    // let values = Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    // outra forma de fracionar o alfabeto //
+    // let alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
+    
+
